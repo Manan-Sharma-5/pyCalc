b96c382c03b91c98b00a69d7449948f913722368
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2025-11-24 21:23:01 +0530

    Added subtraction feature

--- calculator.py
+++ calculator.py
@@ -5,11 +5,15 @@ num2 = float(input("Enter second number: "))
 
 print("Select operation:")
 print("1. Add")
+print("2. Subtract")
 
 choice = input("Enter choice: ")
 
 if choice == "1":
     result = num1 + num2
+    print("Result is:", result)
+elif choice == "2":
+    result = num1 - num2
     print("Result is:", result)
 else:
     print("Invalid choice")
