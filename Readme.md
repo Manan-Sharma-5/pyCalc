0cecc66b2e0f058f2c2d8078791a0a96980d3568
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2025-11-24 21:20:35 +0530

    Initial commit - interactive addition calculator

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Simple Python Calculator
+
+This project performs basic arithmetic operations and is to test git operations.
---
+++ calculator.py
@@ -0,0 +1,15 @@
+print("Welcome to the Interactive Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Select operation:")
+print("1. Add")
+
+choice = input("Enter choice: ")
+
+if choice == "1":
+    result = num1 + num2
+    print("Result is:", result)
+else:
+    print("Invalid choice")
